6bfa8eb19bf7247130a12379205c4e15a0e871a8
chrisonunze <nzeeme@gmail.com>
2023-02-22 07:19:51 -0800

    updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-
-	/**
-	 * main - fibonacci <3
-	 *
-	 * Purpose - no hardcode
-	 *
-	 * Return:  (Success)
-	 */
-
+/**
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
+ */
 
 	int main(void)
 	{
